e62c70cc20b512a87204f1b535d9927acdb0adf8
Joon <77248354+joonsauce@users.noreply.github.com>
2023-01-14 17:43:22 -0500

    reformatted API and updated image receiving code

--- API/api.py
+++ API/api.py
@@ -1,15 +1,24 @@
-from flask import Flask
+import pandas as pd
+import ast
+from flask import Flask, jsonify, request
 from flask_restful import Api, reqparse, Resource
 
 app = Flask(__name__)
-api = Api(app)
 
-class test(Resource):
-    def get(self):
-        data = 1
-        return {'data': data}, 200
+imgData = {'data': str()}
 
-api.add_resource(test, '/test')
+@app.route('/receiveData', methods=['GET'])
+def receiveData():
+    # get image processed by CV algorithm and send back - just a float
+
+    return
+
+# android app needs to send back file in form of {'data': 'base64encodedstring'}
+@app.route('/sendImage', methods=['PUT'])
+def sendImage():
+    imgData['data'] = request.json().to_dict()['data']
+    return '', 200
+
 
 if __name__ == '__main__':
     app.run()
